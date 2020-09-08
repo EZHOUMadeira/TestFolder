2c4757002f983c3497eb0571fe1ad13778510abe
EZHOUMadeira <70650958+EZHOUMadeira@users.noreply.github.com>
2020-09-08 16:32:24 +0800

    Update Main.java

--- TestModule/src/Main.java
+++ TestModule/src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        
+        /*
+        What's happening
+        We declared a string called myString
+        We gave myString the value hello world
+        We re-assigned myString to be hello world
+        We declared And assigned the value of myString1 to Hello world 1
+         */
+
+
+       String myString;
+       myString = "Hello world!";
+       myString = "Hello new world";
+       String myString1 = "Hello world 1";
+
+        System.out.println(myString);
+        System.out.println("i love cs");
+
+        System.out.println(myString1.length());
+        System.out.println(myString1.charAt(12));
     }
 }
