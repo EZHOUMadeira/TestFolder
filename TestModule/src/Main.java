ea4c72d26569a5b090a5cff5d157cd8ae08ec90c
EZHOUMadeira <70650958+EZHOUMadeira@users.noreply.github.com>
2020-09-09 00:35:28 +0800

    Update Main.java

--- TestModule/src/Main.java
+++ TestModule/src/Main.java
@@ -22,10 +22,18 @@ public class Main {
 
         String myString3 = "I am from Beijing";
         System.out.println(myString3);
-        myString3 = "I am from Shanghai";
+        myString3 = "I AM FROM SHANGHAI";
         System.out.println(myString3);
-        
-
+	    
+	
+	   
+        System.out.println("Hi+I am Ellie");
+        System.out.println(myString1.charAt(1));
+        String myString3lower=myString3.toLowerCase();
+        System.out.println(myString3lower);
+	    
+	
+	
 
 	// write your code here
     }
