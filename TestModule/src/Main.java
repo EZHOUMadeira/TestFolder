005ba3e433c872b9fe2a1d1f2c3c7a8cee6b8a64
EZHOUMadeira <70650958+EZHOUMadeira@users.noreply.github.com>
2020-09-11 17:05:47 +0800

    Update Main.java

--- TestModule/src/Main.java
+++ TestModule/src/Main.java
@@ -1,66 +1,10 @@
 package Madeira.cs;
 
-/**
- * Author:Ellie Zhou
- * Date: 9/8/2020
- **/
-
 public class Main {
 
     public static void main(String[] args) {
 
-        String myString1 = "Hi";
-        System.out.println(myString1);
-        myString1 = "bye";
-        System.out.println(myString1);
-
-
-        String myString2 = "I am Ellie";
-        System.out.println(myString2);
-        myString2 = "I am Aashna";
-        System.out.println(myString2);
-
-        String myString3 = "I am from Beijing";
-        System.out.println(myString3);
-        myString3 = "I AM FROM SHANGHAI";
-        System.out.println(myString3);
-	    
-	
-	   
-        System.out.println("Hi+I am Ellie");
-        System.out.println(myString1.charAt(1));
-        String myString3lower=myString3.toLowerCase();
-        System.out.println(myString3lower);
-	
-	    
-	    
-	 /*
-        Lab2
-        Author: Ellie Zhou
-        Date: 9/10
-         */
-
-        Scanner myScanner = new Scanner(System.in);
-        String myScannedInput = myScanner.nextLine();
-        System.out.println("Greetings, "+myScannedInput);
-
-        Scanner myScanner1 = new Scanner(System.in);
-        String myScannedInput1 = myScanner.nextLine();
-        System.out.println(myScannedInput1);
-        System.out.println("One call away, "+myScannedInput1);
-
-        Scanner myScanner2 = new Scanner(System.in);
-        String myScannedInput2 = myScanner.nextLine();
-        System.out.println(myScannedInput2);
-        System.out.println("5666 days, "+myScannedInput2);
-
-
-        /**
-         * Challenge problems
-         * Author: Ellie Zhou
-         * 9/10
-         */
-	
+    
 	
 
 	// write your code here
